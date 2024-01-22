4d415b8e5949ba39c52749cb489d2af59c7b60f3
Jeet Ghosh <jeetghoshsci@gmail.com>
2024-01-22 16:36:43 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,175 @@
+
+const config = {
+
+    "help": [
+        {
+            "title": "help",
+            "description": "for a list of commands(add flags '-d' for commands description)",
+            "info": ["help or ls command gives the list of commands","add '-d' flag for description of all commands also","write command after help to get info about that specific command like 'help typing'"]
+        },
+        {
+            "title": "clear",
+            "description": "to clear the terminal",
+            "info": ["clear or cls command clears the terminal"]
+        },
+        {
+            "title": "about",
+            "description": "to learn more about me",
+            "info": ["use about command to learn more about me"]
+        },
+        {
+            "title": "social",
+            "description": "to see my social links (add flags '-l' for links and '-d' for detailed results)",
+            "info": ["use social command to see my social links","add '-l' flag for links and '-d' flag for detailed results"]
+        },
+        {
+            "title": "projects",
+            "description": "to see my projects",
+            "info": ["use projects command to see my projects"]
+        },
+        {
+            "title": "blogs",
+            "description": "to see my recent blogs",
+            "info": ["use blogs command to see my recent blogs"]
+        },
+        {
+            "title": "contact",
+            "description": "to enquire about my services",
+            "info": ["use enquire command to enquire about my services"]
+        },
+        {
+            "title": "cheer",
+            "description": "to appreciate my work",
+            "info": ["use cheer command to appreciate my work"]
+        },
+        {
+            "title": "repos",
+            "description": "to see my github repositories",
+            "info": ["use repos command to see my github repositories"]
+        },
+        {
+            "title": "ipconfig",
+            "description": "to see your IP details",
+            "info": ["use ipconfig command to see your IP details"]
+        },
+        {
+            "title": "github",
+            "description": "to see my github stats",
+            "info": ["use github command to see my github stats"]
+        },
+        {
+            "title": "contributors",
+            "description": "to see all the contributors",
+            "info": ["use contributors command to see all the contributors"]
+        },
+        {
+            "title": "download",
+            "description": "to download my pdf resume",
+            "info": ["use download command to download my pdf resume"]
+        },
+        {
+            "title": "calc",
+            "description": "to evaluate an expression, for eg: (2 + 3)",
+            "info": ["use calc command to evaluate an expression","for eg: write 'calc 2+6*5/3'"]
+        },
+        {
+            "title": "experience",
+            "description": "to see my work experience",
+            "info": ["use experience command to see my work experience"]
+        },
+        {
+            "title": "history",
+            "description": "shows the last 10 valid commands performed, use --clear flag to clear the history",
+            "info": ["use history command to show your last 10 commands history","use --clear flag to clear the history","use history {id} command to run command of that id in your history"]
+        },
+        {
+            "title": "skills",
+            "description": "to see my skills",
+            "info": ["use skills command to see my skills"]
+        },
+        {
+            "title": "typing",
+            "description": "shows typing animation status",
+            "info": ["use typing command to see typing animation status","Turn typing animation on and off by adding -on or -off flags respectively","Also u can write a number(in ms) to set typing custom animation speed"]
+        },
+        {
+            "title": "reset",
+            "description": "to reload site",
+            "info": ["use reset command to to reload site"]
+        }
+    ],
+    "terminal": {
+        "user": "$Jeet Ghosh",
+        "host": "sudo",
+        "path": "~/guest"
+    },
+    "blogs": [
+        {
+            "site": "Key2ai",
+            "url": "https://key2ai.co/blogs"
+        }
+    ],
+    "cheer": {
+        "responseArray": [
+            "Thank you! It makes my day😊😊😊",
+            "It is great to hear that way!😁😁😁",
+            "I would love to take credit😂😂😂",
+            "That's so good to hear! I'm glad😍😍😍"
+        ]
+    },
+    "about": "🎓 Aspiring software engineer with strong foundation in computer science and engineering, gained through Btech degree program and hands-on experience with various technologies. (Graduating July 2026) \n 🌇 Proficient in Devops, data structures and algorithms, SQL, and Google Cloud Platform. Demonstrated expertise in these technologies through successful projects. \n 📚 Active member of tech community, serving as lead for multiple organizations including GDSC, and Hackiton Community. Contributed to growth and success of these organizations through leadership and technical skills. \n 💻 Constantly seeking opportunities to grow and improve skills, with long-term goal of becoming a software development engineer. As a Tech Geek, demonstrated commitment to excellence and potential to succeed in tech industry.",
+    "social": [
+        {
+            "title": "Github",
+            "link": "https://github.com/Jeet-programmer"
+        },
+        {
+            "title": "LinkedIn",
+            "link": "https://www.linkedin.com/in/jeet-ghosh-tech/",
+            "connections": "300+"
+        },
+        {
+            "title": "GeeksforGeeks",
+            "link": "https://auth.geeksforgeeks.org/user/jeet_ghosh"
+        }
+    ],
+    "projects": [
+        {
+            "title": "Sachai",
+            "link": "https://github.com/TechSpiritSS/Mood-Messenger",
+            "description": "Mood Messenger is a real-time chat application built using Node.js, React.js, and Cloud SQL. It allows users to sign in with their Google account, initiate chats with other registered users based on their email, send messages, and view previous chats."
+        },
+        {
+            "title": "Sanket",
+            "link": "https://github.com/TechSpiritSS/PregChat_Dialogflow",
+            "description": "PregChat is a chatbot designed to help expecting mothers access important information about their pregnancy in a simple, accessible and interactive way. Our chatbot is powered by Dialogflow, a Google Cloud-based natural language processing platform. With PregChat, mothers can easily ask about their symptoms and receive information regarding medical emergencies, first aid, medications, and exercises. PregChat aims to provide support and education to mothers in need, ultimately improving the health outcomes for both mother and child."
+        }
+    ],
+    "contact": {
+        "email": "jeetghoshsci@gmail.com"
+    },
+    "experience": [
+        {
+            "title": "WebDev Intern at Achanitya Web Solutions",
+            "description": "▪️ Improved performance and maintainability of client-team management application, Redwing, through refactoring of static code into dynamic function calls and reduction of team page code by 70%. \n ▪️ Identified and fixed drag event bug in Nodle Project that caused dramatic increase in CPU utilization (from 7% to 73% with a single mouse drag on flowchart area). \n ▪️ Resolved complex SVG scaling issue in Anatomy-Mapper Project, ensuring accuracy and reliability of visualization."
+        }
+    ],
+    "skills": [
+        {
+            "title": "Technical skills are: ",
+        "description": "Python, Flask, FastAPI, Tenserflow, Transformers C, MySQL, Linux, JSON, DSA, Google Cloud Platform, Git, Material UI, Bootstrap, Tailwind"
+        },
+        {
+            "title": "Other Skills are:",
+            "description": "Problem Solving, Team Leadership, Community Management, Team Building"
+        }
+    ],
+    "neofetch": {
+        "name": "Jeet Ghosh",
+        "title": "MERN Developer",
+        "skills": "Frontend, Backend, Cloud",
+        "shell": "zsh",
+        "languages": "Javascript, C++, HTML/CSS, SQL"
+    },
+}
+export default config
